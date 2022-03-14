ef90359e84edf762be7841511cf18bb72785805f
Morgan Askins <maskins@ucdavis.edu>
2022-03-14 10:55:17 -0700

    corrected ignore

--- RatpacConfig.cmake
+++
@@ -1,4 +0,0 @@
-# Similar to ratpac.sh.in
-set(RATROOT /home/morgan/ratpacSetup/ratpac/build)
-set(RATSHARE /home/morgan/ratpacSetup/ratpac)
-set(ROOT_INCLUDE_PATH /home/morgan/ratpacSetup/ratpac/build/include)
