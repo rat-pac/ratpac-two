062b94427d98e3c884db5e0b548f5727f67d2c16
Morgan Askins <maskins@ucdavis.edu>
2022-03-14 10:47:09 -0700

    Initial cmake config for external library

---
+++ RatpacConfig.cmake
@@ -0,0 +1,4 @@
+# Similar to ratpac.sh.in
+set(RATROOT /home/morgan/ratpacSetup/ratpac/build)
+set(RATSHARE /home/morgan/ratpacSetup/ratpac)
+set(ROOT_INCLUDE_PATH /home/morgan/ratpacSetup/ratpac/build/include)
