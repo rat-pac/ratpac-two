2e9c96d5f8a20e9d3f22bc30255e37d176310619
Morgan Askins <maskins@ucdavis.edu>
2023-10-18 16:22:04 -0700

    more verbosity and smart formatter

--- cformat.sh
+++ cformat.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-find ./src \( -name "*.cc" -o -name "*.hh" -o -name "*.icc" -o -name "*.cpp" \) -exec clang-format --style=file -i {} \;
+echo "Checking formatting..."
+changes=0
+for file in `find ./src \( -name "*.cc" -o -name "*.hh" -o -name "*.icc" -o -name "*.cpp" \)`;
+do
+    retval=`clang-format -style=file -n -Werror $file`
+    if [ $? -eq 1 ]; then
+        echo "Formatting $file"
+        clang-format -style=file -i $file
+        changes=1
+    fi
+done
+
+if [ $changes -eq 0 ]; then
+    exit 0
+else
+    echo "Formatting changes made, please commit them"
+    exit 1
+fi
