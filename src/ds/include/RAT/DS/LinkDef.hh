91fda5f1e66d1d0272c3bb95bd4c080af00f0b27
James Shen <jamesjieranshen@gmail.com>
2024-08-15 16:50:31 -0400

    Add ChannelStatus class

--- src/ds/include/RAT/DS/LinkDef.hh
+++ src/ds/include/RAT/DS/LinkDef.hh
@@ -2,6 +2,7 @@
 
 #pragma link C++ class RAT::DS::Root + ;
 #pragma link C++ class RAT::DS::PMTInfo + ;
+#pragma link C++ class RAT::DS::ChannelStatus + ;
 
 #pragma link C++ class RAT::DS::MC + ;
 #pragma link C++ class RAT::DS::MCParticle + ;
@@ -58,6 +59,7 @@
 
 #pragma link C++ class vector < RAT::DS::Root>;
 #pragma link C++ class vector < RAT::DS::PMTInfo>;
+#pragma link C++ class vector < RAT::DS::ChannelStatus>;
 
 #pragma link C++ class vector < RAT::DS::MC>;
 #pragma link C++ class vector < RAT::DS::MCParticle>;
