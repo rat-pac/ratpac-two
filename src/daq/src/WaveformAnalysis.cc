f1e2f6ecd3fd9b0f410423c1f7334c673c5f0c1e
James Shen <jamesjieranshen@gmail.com>
2024-08-22 18:36:39 -0400

    Do not perform analysis on broken channels

--- src/daq/src/WaveformAnalysis.cc
+++ src/daq/src/WaveformAnalysis.cc
@@ -439,6 +439,7 @@ Processor::Result WaveformAnalysis::Event(DS::Root* ds, DS::EV* ev) {
   for (int pmt_id : pmt_ids) {
     // Do not analyze negative pmtid channels, since they do not correspond to real PMTs.
     if (pmt_id < 0) continue;
+    if (!ch_status.GetOnlineByPMTID(pmt_id)) continue;
     DS::DigitPMT* digitpmt = ev->GetDigitPMT(pmt_id);
     double time_offset = fApplyCableOffset ? ch_status.GetCableOffsetByPMTID(pmt_id) : 0.0;
     RunAnalysis(digitpmt, pmt_id, dsdigit, time_offset);
