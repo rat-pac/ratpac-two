7cd30612ad2aa67a80b10620baeae2edc75f2e4c
James Shen <jamesjieranshen@gmail.com>
2024-12-09 15:09:20 -0500

    add validity guard to fitcentroid test

--- test/full/fitcentroid/fitcentroid.C
+++ test/full/fitcentroid/fitcentroid.C
@@ -12,12 +12,12 @@ void fitcentroid(std::string event_filename, std::string out_filename) {
       new TProfile("hReconVsEnergy",
                    "Electrons at center (error bars are RMS);Kinetic energy (MeV);Reconstructed X (mm)", 5, 3, 15, "s");
 
-  T->Draw("ds.ev.fitResults.fit_position.fX>>hCenterRes", "ds.mc.particle.ke < 0.0205 && ds.mc.particle.pos.fX < 0.05",
-          "goff");
+  T->Draw("ds.ev.fitResults.fit_position.fX>>hCenterRes",
+          "ds.mc.particle.ke < 0.0205 && ds.mc.particle.pos.fX < 0.05 && ds.ev.fitResults.valid_position", "goff");
   T->Draw("ds.ev.fitResults.fit_position.fX-ds.mc.particle.pos.fX:(ds.mc.particle.pos.Mag()/437)>>hReconVsRadius",
-          "ds.mc.particle.ke < 0.0205 && ds.mc.particle.pos.fX > 0.05", "goff prof");
+          "ds.mc.particle.ke < 0.0205 && ds.mc.particle.pos.fX > 0.05 && ds.ev.fitResults.valid_position", "goff prof");
   T->Draw("ds.ev.fitResults.fit_position.fX:ds.mc.particle.ke>>hReconVsEnergy",
-          "ds.mc.particle.ke > 0.0205 && ds.mc.particle.pos.fX < 0.05", "goff prof");
+          "ds.mc.particle.ke > 0.0205 && ds.mc.particle.pos.fX < 0.05 && ds.ev.fitResults.valid_position", "goff prof");
 
   hCenterRes->Fit("gaus");
   hReconVsRadius->SetMinimum(-2000);
