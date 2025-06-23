d11aa7f8f63d3e8ef4d96987b9466c08da5f3fee
srig19 <sgadamsetty6@berkeley.edu>
2025-06-23 10:34:31 -0700

    Removing binning in energy space and verifying truth information reproduces measured spectra

--- src/gen/src/VertexGen_Laserball.cc
+++ src/gen/src/VertexGen_Laserball.cc
@@ -33,20 +33,6 @@ void VertexGen_Laserball::GeneratePrimaryVertex(G4Event *event, G4ThreeVector &d
   double phi;
   double wavelength;
   double energy;
-  bool wl_binning = true;
-  info << "Starting GeneratePrimaryVertex()" << newline;
-  DBLinkPtr spectraparams = DB::Get()->GetLink("WLSPECTRA", "params");
-
-  //"equal_bins" controls whether the spectrum is equally binned in wavelength or energy space
-  if (spectraparams->GetS("equal_bins") == "wavelength") {
-    wl_binning = true;
-  } else if (spectraparams->GetS("equal_bins") == "energy") {
-    wl_binning = false;
-  } else {
-    Log::Die("VertexGen_Laserball: Invalid binning option selected.");
-    info << "VertexGen_Laserball: Invalid binning option selected." << newline;
-  }
-  info << "Equal binning method selected" << newline;
 
   for (int i = 0; i < fNumPhotons; i++) {
     theta = acos(2.0 * G4UniformRand() - 1.0);
@@ -56,34 +42,19 @@ void VertexGen_Laserball::GeneratePrimaryVertex(G4Event *event, G4ThreeVector &d
       spectradb = DB::Get()->GetLink("WLSPECTRA", fWavelengthIndex);
     } catch (DBNotFoundError &e) {
       Log::Die("VertexGen_Laserball: Wavelength simulated does not have measured spectrum.");
-      info << "VertexGen_Laserball: Wavelength simulated does not have measured spectrum." << newline;
     }
 
     try {
-      if (wl_binning) {
-        double wl_average = spectradb->GetD("avg_wl");
-        std::vector<double> wls = spectradb->GetDArray("dist_wl");
-        std::vector<double> wl_probs = spectradb->GetDArray("dist_wl_intensity");
-        if (wls.size() != wl_probs.size()) {
-          Log::Die("VertexGen_Laserball: Wavelength and probability arrays have different length");
-          info << "VertexGen_Laserball: Wavelength and probability arrays have different length" << newline;
-        }
-        wavelength = pickValue(wls, wl_probs, wl_average);
-        energy = CLHEP::hbarc * CLHEP::twopi / (wavelength * CLHEP::nm);
-      } else {
-        double e_average = spectradb->GetD("avg_e");
-        std::vector<double> es = spectradb->GetDArray("dist_e");
-        std::vector<double> e_probs = spectradb->GetDArray("dist_e_intensity");
-        if (es.size() != e_probs.size()) {
-          Log::Die("VertexGen_Laserball: Energy and probability arrays have different length.");
-          info << "VertexGen_Laserball: Energy and probability arrays have different length." << newline;
-        }
-        energy = pickValue(es, e_probs, e_average);
+      double wl_average = spectradb->GetD("avg_wl");
+      std::vector<double> wls = spectradb->GetDArray("dist_wl");
+      std::vector<double> wl_probs = spectradb->GetDArray("dist_wl_intensity");
+      if (wls.size() != wl_probs.size()) {
+        Log::Die("VertexGen_Laserball: Wavelength and probability arrays have different length");
       }
-      info << "Wavelength for photon selected" << newline;
+      wavelength = pickValue(wls, wl_probs, wl_average);
+      energy = CLHEP::hbarc * CLHEP::twopi / (wavelength * CLHEP::nm);
     } catch (DBNotFoundError &e) {
       Log::Die("VertexGen_Laserball: Error with retrieving wavelength from spectrum.");
-      info << "VertexGen_Laserball: Error with retrieving wavelength from spectrum." << newline;
     }
     mom.setRThetaPhi(energy, theta, phi);  // Momentum == energy units in GEANT4
     // Distribute times expoenentially, but don't bother picking a
@@ -100,7 +71,6 @@ void VertexGen_Laserball::GeneratePrimaryVertex(G4Event *event, G4ThreeVector &d
     G4ThreeVector pol = e1 * cos(phi) + e2 * sin(phi);
     photon->SetPolarization(pol.x(), pol.y(), pol.z());
     photon->SetMass(0.0);  // Seems odd, but used in GLG4VertexGen_Gun
-    info << "Photon characterists set" << newline;
 
     vertex->SetPrimary(photon);
     event->AddPrimaryVertex(vertex);
@@ -116,16 +86,13 @@ void VertexGen_Laserball::SetState(G4String newValues) {
          << newline;
     return;
   }
-  info << "Starting SetState()" << newline;
   istringstream is(newValues.c_str());
   int num;
   string wavelength;
   is >> num >> wavelength;
-  info << "Num and Wavelength set in SetState()" << newline;
 
   if (is.fail()) {
     Log::Die("VertexGen_Laserball: Vertex state string incorrectly formatted.");
-    info << "VertexGen_Laserball: Vertex state string incorrectly formatted." << newline;
   } else {
     fWavelengthIndex = wavelength;
   }
@@ -134,7 +101,6 @@ void VertexGen_Laserball::SetState(G4String newValues) {
   is >> exp;
   if (exp < 0.0) {
     Log::Die("VertexGen_Laserball: Exponential time constant must be positive");
-    info << "VertexGen_Laserball: Exponential time constant must be positive." << newline;
   }
 
   fNumPhotons = num;
@@ -145,7 +111,7 @@ G4String VertexGen_Laserball::GetState() { return dformat("%d\t%f\t%f", fNumPhot
 
 double VertexGen_Laserball::pickValue(vector<double> values, vector<double> probs, double avg) {
   double integral = 0.0;
-  std::vector<double> probCumu = std::vector<double>(values.size());
+  vector<double> probCumu = vector<double>(values.size());
   probCumu[0] = 0.0;
   for (size_t i = 0; i < values.size() - 1; i++) {
     integral += (values[i + 1] - values[i]) * (probs[i] + probs[i + 1]) / 2.0;  // trapezoid integration
@@ -162,6 +128,6 @@ double VertexGen_Laserball::pickValue(vector<double> values, vector<double> prob
              values[i - 1];  // linear interpolation
     }
   }
-  info << "VertexGen_Laserball::pickValue: impossible condition encountered - returning average value" << newline;
+  Log::Die("VertexGen_Laserball::pickValue: impossible condition encountered");
   return avg;
 }
