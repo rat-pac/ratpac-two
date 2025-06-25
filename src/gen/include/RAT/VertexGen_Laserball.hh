87a71fab4dfd902e04897ca1b491f667db3c5c0f
srig19 <sgadamsetty6@berkeley.edu>
2025-06-25 10:36:43 -0700

    Resolving comments

--- src/gen/include/RAT/VertexGen_Laserball.hh
+++ src/gen/include/RAT/VertexGen_Laserball.hh
@@ -5,25 +5,25 @@
 
 #include <RAT/GLG4VertexGen.hh>
 
-using namespace RAT;
-using namespace std;
+namespace RAT {
 
 class VertexGen_Laserball : public GLG4VertexGen {
  public:
   VertexGen_Laserball(const char *arg_dbname = "laserball");
-  virtual ~VertexGen_Laserball();
+  virtual ~VertexGen_Laserball(){};
   virtual void GeneratePrimaryVertex(G4Event *argEvent, G4ThreeVector &dx, G4double dt);
   /** State format "num_photons wavelength_nm" */
   virtual void SetState(G4String newValues);
   virtual G4String GetState();
-  double pickValue(vector<double> values, vector<double> probs, double avg);
+  double pickWavelength(std::vector<double> &values, std::vector<double> &probs);
 
  private:
   G4ParticleDefinition *fOpticalPhoton;
-  int fNumPhotons;
-  string fMaterial;
+  size_t fNumPhotons;
   double fExpTime;
-  string fWavelengthIndex;
+  std::string fWavelengthIndex;
 };
+
+}  // namespace RAT
 
 #endif
--- src/gen/src/VertexGen_Laserball.cc
+++ src/gen/src/VertexGen_Laserball.cc
@@ -12,19 +12,14 @@
 #include <RAT/VertexGen_Laserball.hh>
 #include <Randomize.hh>
 
-using namespace RAT;
-using namespace std;
+namespace RAT {
 
 VertexGen_Laserball::VertexGen_Laserball(const char *arg_dbname) : GLG4VertexGen(arg_dbname) {
   fOpticalPhoton = G4ParticleTable::GetParticleTable()->FindParticle("opticalphoton");
-  fMaterial = "";
   fWavelengthIndex = "";
   fNumPhotons = 0;
   fExpTime = 0;
-  SetState("1 408nm");  // one photon per event, 408 nm
 }
-
-VertexGen_Laserball::~VertexGen_Laserball() {}
 
 void VertexGen_Laserball::GeneratePrimaryVertex(G4Event *event, G4ThreeVector &dx, G4double dt) {
   // Pick direction isotropically
@@ -33,29 +28,33 @@ void VertexGen_Laserball::GeneratePrimaryVertex(G4Event *event, G4ThreeVector &d
   double phi;
   double wavelength;
   double energy;
+  DBLinkPtr spectradb;
+  std::vector<double> wls;
+  std::vector<double> wl_probs;
 
-  for (int i = 0; i < fNumPhotons; i++) {
+  try {
+    spectradb = DB::Get()->GetLink("LASERBALL", fWavelengthIndex);
+  } catch (DBNotFoundError &e) {
+    Log::Die("VertexGen_Laserball: Wavelength simulated does not have measured spectrum.");
+  }
+
+  try {
+    wls = spectradb->GetDArray("wavelength");
+    wl_probs = spectradb->GetDArray("intensity");
+    if (wls.size() != wl_probs.size()) {
+      Log::Die("VertexGen_Laserball: Wavelength and probability arrays have different length");
+    }
+  } catch (DBNotFoundError &e) {
+    Log::Die("VertexGen_Laserball: Error with retrieving wavelength from spectrum.");
+  }
+
+  for (size_t i = 0; i < fNumPhotons; i++) {
     theta = acos(2.0 * G4UniformRand() - 1.0);
     phi = 2.0 * G4UniformRand() * CLHEP::pi;
-    DBLinkPtr spectradb;
-    try {
-      spectradb = DB::Get()->GetLink("WLSPECTRA", fWavelengthIndex);
-    } catch (DBNotFoundError &e) {
-      Log::Die("VertexGen_Laserball: Wavelength simulated does not have measured spectrum.");
-    }
 
-    try {
-      double wl_average = spectradb->GetD("avg_wl");
-      std::vector<double> wls = spectradb->GetDArray("dist_wl");
-      std::vector<double> wl_probs = spectradb->GetDArray("dist_wl_intensity");
-      if (wls.size() != wl_probs.size()) {
-        Log::Die("VertexGen_Laserball: Wavelength and probability arrays have different length");
-      }
-      wavelength = pickValue(wls, wl_probs, wl_average);
-      energy = CLHEP::hbarc * CLHEP::twopi / (wavelength * CLHEP::nm);
-    } catch (DBNotFoundError &e) {
-      Log::Die("VertexGen_Laserball: Error with retrieving wavelength from spectrum.");
-    }
+    wavelength = pickWavelength(wls, wl_probs);
+    energy = CLHEP::hbarc * CLHEP::twopi / (wavelength * CLHEP::nm);
+
     mom.setRThetaPhi(energy, theta, phi);  // Momentum == energy units in GEANT4
     // Distribute times expoenentially, but don't bother picking a
     // random number if there is no time constant
@@ -78,24 +77,17 @@ void VertexGen_Laserball::GeneratePrimaryVertex(G4Event *event, G4ThreeVector &d
 }
 
 void VertexGen_Laserball::SetState(G4String newValues) {
-  if (newValues.length() == 0) {
-    // print help and current state
-    info << "Current state of this VertexGen_Laserball:" << newline << " \"" << GetState() << "\"" << newline
-         << newline;
-    info << "Format of argument to VertexGen_Laserball::SetState: " << newline << " \"num_photons wavelength_nm\"\n"
-         << newline;
-    return;
-  }
-  istringstream is(newValues.c_str());
+  std::istringstream is(newValues.c_str());
   int num;
-  string wavelength;
+  std::string wavelength;
   is >> num >> wavelength;
 
   if (is.fail()) {
-    Log::Die("VertexGen_Laserball: Vertex state string incorrectly formatted.");
-  } else {
-    fWavelengthIndex = wavelength;
+    Log::Die(
+        "VertexGen_Laserball: Vertex state string incorrectly formatted. Format of argument to "
+        "VertexGen_Laserball::SetState: \"num_photons wavelength[nm]nm\"\n");
   }
+  fWavelengthIndex = wavelength;
 
   double exp = 0.0;
   is >> exp;
@@ -109,11 +101,20 @@ void VertexGen_Laserball::SetState(G4String newValues) {
 
 G4String VertexGen_Laserball::GetState() { return dformat("%d\t%f\t%f", fNumPhotons, fExpTime, fWavelengthIndex); }
 
-double VertexGen_Laserball::pickValue(vector<double> values, vector<double> probs, double avg) {
+double VertexGen_Laserball::pickWavelength(std::vector<double> &values, std::vector<double> &probs) {
+  if (probs[0] != 0) {
+    Log::Die("VertexGen_Laserball::pickWavelength: Leading entry in intensity is not 0");
+  }
   double integral = 0.0;
-  vector<double> probCumu = vector<double>(values.size());
+  std::vector<double> probCumu = std::vector<double>(values.size());
   probCumu[0] = 0.0;
+
   for (size_t i = 0; i < values.size() - 1; i++) {
+    if (probs[i] < 0) {
+      Log::Die("VertexGen_Laserball::pickWavelength: An intensity is negative");
+    } else if (i != 0 && probs[i] == 0) {
+      Log::Die("VertexGen_Laserball::pickWavelength: Non-leading entry in intensity is 0");
+    }
     integral += (values[i + 1] - values[i]) * (probs[i] + probs[i + 1]) / 2.0;  // trapezoid integration
     probCumu[i + 1] = integral;
   }
@@ -121,13 +122,38 @@ double VertexGen_Laserball::pickValue(vector<double> values, vector<double> prob
     probs[i] /= integral;
     probCumu[i] /= integral;
   }
+
   double rval = G4UniformRand();
-  for (size_t i = 1; i < values.size(); i++) {
-    if (rval <= probCumu[i]) {
-      return (rval - probCumu[i - 1]) * (values[i] - values[i - 1]) / (probCumu[i] - probCumu[i - 1]) +
-             values[i - 1];  // linear interpolation
+
+  // Check edge cases first
+
+  if (rval < probCumu[1]) {
+    return rval * (values[1] - values[0]) / (probCumu[1]) + values[0];
+  } else if (rval == 1) {
+    return values[values.size() - 1];
+  } else {
+    int low = 0;
+    int high = probCumu.size() - 1;
+    while (low <= high) {
+      int mid = low + (high - low) / 2;
+
+      // Check if x is present at mid
+      if (rval <= probCumu[mid] && rval > probCumu[mid - 1]) {
+        return (rval - probCumu[mid - 1]) * (values[mid] - values[mid - 1]) / (probCumu[mid] - probCumu[mid - 1]) +
+               values[mid - 1];
+      }
+      // If x greater, ignore left half
+      if (rval > probCumu[mid]) {
+        low = mid + 1;
+      }
+      // If x is smaller, ignore right half
+      else {
+        high = mid - 1;
+      }
     }
   }
-  Log::Die("VertexGen_Laserball::pickValue: impossible condition encountered");
-  return avg;
+
+  Log::Die("VertexGen_Laserball::pickWavelength: impossible condition encountered");
 }
+
+}  // namespace RAT
