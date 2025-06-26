e1b6873a52cc948f9e9aa0c59327850dbc2bb691
Max Generowicz <git-mdcg@my-post-office.net>
2025-06-26 18:30:59 +0100

    Move bulky quenching calculator instantiation to separate method

--- src/core/src/Trajectory.cc
+++ src/core/src/Trajectory.cc
@@ -15,87 +15,16 @@ namespace RAT {
 
 G4Allocator<Trajectory> aTrajectoryAllocator;
 bool Trajectory::fgDoAppendMuonStepSpecial = false;
+QuenchingCalculator* BuildQuenchingCalculator();
 
 Trajectory::Trajectory() : G4Trajectory() {
   ratTrack = new DS::MCTrack;
-
-  // Instantiate the quenching model
-  RAT::DB *db = RAT::DB::Get();
-  RAT::DBLinkPtr tbl = db->GetLink("QUENCHING");
-  std::string selection = tbl->GetS("model");
-  BirksLaw model;
-  if (selection == "birks") {
-    model = BirksLaw();
-  } else {
-    // no such quenching model
-    std::string msg = "Invalid quenching model: " + selection;
-    RAT::Log::Die(msg);
-  }
-  std::string strategy = tbl->GetS("strategy");
-  if (strategy == "naive") {
-    this->fQuenching = new NaiveQuenchingCalculator(model);
-  } else if (strategy == "integrated") {
-    std::string method = tbl->GetS("integration");
-    Quadrature *quadrature;
-    if (method == "fixed") {
-      // TODO
-      double resolution = tbl->GetD("resolution");
-      quadrature = new FixedTrapezoidalQuadrature(resolution);
-    } else if (method == "adaptive") {
-      double tolerance = tbl->GetD("tolerance");
-      quadrature = new AdaptiveSimpsonQuadrature(tolerance);
-    } else {
-      // no such integration method
-      std::string msg = "Invalid integration method: " + method;
-      RAT::Log::Die(msg);
-    }
-    this->fQuenching = new IntegratedQuenchingCalculator(model, quadrature);
-  } else {
-    // no such quenching calculation strategy
-    std::string msg = "Invalid quenching calculation strategy: " + strategy;
-    RAT::Log::Die(msg);
-  }
+  this->fQuenching = BuildQuenchingCalculator();
 }
 
 Trajectory::Trajectory(const G4Track *aTrack) : G4Trajectory(aTrack), creatorProcessName("start") {
   ratTrack = new DS::MCTrack;
-
-  // Instantiate the quenching model
-  RAT::DB *db = RAT::DB::Get();
-  RAT::DBLinkPtr tbl = db->GetLink("QUENCHING");
-  std::string selection = tbl->GetS("model");
-  BirksLaw model;
-  if (selection == "birks") {
-    model = BirksLaw();
-  } else {
-    // no such quenching model
-    std::string msg = "Invalid quenching model: " + selection;
-    RAT::Log::Die(msg);
-  }
-  std::string strategy = tbl->GetS("strategy");
-  if (strategy == "naive") {
-    this->fQuenching = new NaiveQuenchingCalculator(model);
-  } else if (strategy == "integrated") {
-    std::string method = tbl->GetS("integration");
-    Quadrature *quadrature;
-    if (method == "fixed") {
-      // TODO
-      double resolution = tbl->GetD("resolution");
-      quadrature = new FixedTrapezoidalQuadrature(resolution);
-    } else if (method == "adaptive") {
-      double tolerance = tbl->GetD("tolerance");
-      quadrature = new AdaptiveSimpsonQuadrature(tolerance);
-    } else {
-      // no such integration method
-      std::string msg = "Invalid integration method: " + method;
-      RAT::Log::Die(msg);
-    }
-    this->fQuenching = new IntegratedQuenchingCalculator(model, quadrature);
-  } else {
-    // no such quenching calculation strategy
-    std::string msg = "Invalid quenching calculation strategy: " + strategy;
-    RAT::Log::Die(msg);
-  }
+  this->fQuenching = BuildQuenchingCalculator();
 
   ratTrack->SetID(GetTrackID());
   ratTrack->SetParentID(GetParentID());
@@ -116,6 +45,47 @@ Trajectory::Trajectory(const G4Track *aTrack) : G4Trajectory(aTrack), creatorPro
 Trajectory::~Trajectory() {
   delete ratTrack;
   delete fQuenching;
+}
+
+QuenchingCalculator* BuildQuenchingCalculator() {
+  QuenchingCalculator* quenching_calculator;
+
+  RAT::DB *db = RAT::DB::Get();
+  RAT::DBLinkPtr tbl = db->GetLink("QUENCHING");
+  std::string selection = tbl->GetS("model");
+  BirksLaw model;
+  if (selection == "birks") {
+    model = BirksLaw();
+  } else {
+    // no such quenching model
+    std::string msg = "Invalid quenching model: " + selection;
+    RAT::Log::Die(msg);
+  }
+  std::string strategy = tbl->GetS("strategy");
+  if (strategy == "naive") {
+    quenching_calculator = new NaiveQuenchingCalculator(model);
+  } else if (strategy == "integrated") {
+    std::string method = tbl->GetS("integration");
+    Quadrature *quadrature;
+    if (method == "fixed") {
+      // TODO
+      double resolution = tbl->GetD("resolution");
+      quadrature = new FixedTrapezoidalQuadrature(resolution);
+    } else if (method == "adaptive") {
+      double tolerance = tbl->GetD("tolerance");
+      quadrature = new AdaptiveSimpsonQuadrature(tolerance);
+    } else {
+      // no such integration method
+      std::string msg = "Invalid integration method: " + method;
+      RAT::Log::Die(msg);
+    }
+    quenching_calculator = new IntegratedQuenchingCalculator(model, quadrature);
+  } else {
+    // no such quenching calculation strategy
+    std::string msg = "Invalid quenching calculation strategy: " + strategy;
+    RAT::Log::Die(msg);
+  }
+  return quenching_calculator;
 }
 
 void Trajectory::AppendStep(const G4Step *aStep) {
