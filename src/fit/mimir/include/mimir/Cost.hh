222d92cb3eabc1acef5c7365cfa5bf70962a0a88
James Shen <jamesjieranshen@gmail.com>
2025-09-22 15:48:41 -0400

    Move AddHit logic from FitStrategy to Cost

--- src/fit/mimir/include/mimir/Cost.hh
+++ src/fit/mimir/include/mimir/Cost.hh
@@ -1,5 +1,6 @@
 #pragma once
 #include <RAT/DB.hh>
+#include <RAT/FitterInputHandler.hh>
 #include <mimir/Common.hh>
 #include <mimir/ParamSet.hh>
 
@@ -11,25 +12,20 @@ class Cost {
   virtual bool Configure(RAT::DBLinkPtr db_link) = 0;
   virtual double operator()(const ParamSet& params) const = 0;
 
-  void SetHits(const std::vector<int>& pmtids, const std::vector<double>& _times = {},
-               const std::vector<double>& _charges = {}) {
-    std::vector<double> times = _times;
-    std::vector<double> charges = _charges;
-    // allow these values to be empty so that cost functions can be evaluated on just hit channels.
-    if (!_times.size()) times.resize(pmtids.size(), INVALID);
-    if (!_charges.size()) charges.resize(pmtids.size(), INVALID);
-    hit_pmtids = pmtids;
-    hit_times = times;
-    hit_charges = charges;
-  }
-
-  void AddHit(const int pmtid, const double time = INVALID, const double charge = INVALID) {
+  virtual void AddHit(const int pmtid, const RAT::FitterInputHandler& input_handler) {
     hit_pmtids.push_back(pmtid);
-    hit_times.push_back(time);
-    hit_charges.push_back(charge);
+    hit_times.push_back(input_handler.GetTime(pmtid));
+    hit_charges.push_back(input_handler.GetCharge(pmtid));
   }
 
-  void ClearHits() {
+  virtual void AddAllHits(const RAT::FitterInputHandler& input_handler) {
+    std::vector<int> pmtids = input_handler.GetAllHitPMTIDs();
+    for (int pmtid : pmtids) {
+      AddHit(pmtid, input_handler);
+    }
+  }
+
+  virtual void ClearHits() {
     hit_pmtids.clear();
     hit_times.clear();
     hit_charges.clear();
