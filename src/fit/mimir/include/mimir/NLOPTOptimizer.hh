e57e96f8d9c988608fe38c29fed0100fea0d079e
Ravi Carpen Pitelka <66141981+rpitelka@users.noreply.github.com>
2025-11-12 23:35:29 -0500

    Add NLOPT as a Mimir optimizer (#353)
    * Change Mimir fitter name setting logic
    
    * Add NLOPT support
    
    * Add an NLOPT optimizer to Mimir
    
    * Update nlopt algorithm selection interface
    
    * Update comments and documentation
    
    * Ensure mimir nlopt optimizer only compiles if NLOPT is enabled
    
    * Revert Mimir fitter name setting logic
    
    * Implementation fixes
    
    * Change C style methods to C++ style
    
    * Remove C header
    
    * Overload parameter update function with C-style interface to improve NLOPT performance

---
+++ src/fit/mimir/include/mimir/NLOPTOptimizer.hh
@@ -0,0 +1,22 @@
+#include <RAT/Config.hh>
+#if NLOPT_Enabled
+#pragma once
+#include <mimir/Factory.hh>
+#include <mimir/Optimizer.hh>
+#include <nlopt.hpp>
+#include <string>
+
+namespace Mimir {
+
+class NLOPTOptimizer : public Optimizer {
+ public:
+  bool Configure(RAT::DBLinkPtr db_link) override;
+  void MinimizeImpl(std::function<double(const ParamSet&)> cost, ParamSet& params) override;
+
+ protected:
+  std::string fAlgoType;
+  int fMaxEval;
+  double fTolerance;
+};
+}  // namespace Mimir
+#endif
