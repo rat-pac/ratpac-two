e1240de40faf0c51bbb8afd2dd7796dee8570040
Ravi Carpen Pitelka <66141981+rpitelka@users.noreply.github.com>
2026-09-10 18:13:53 -0400

    Enforce absolute path requirement for rattest experiment binaries (#431)
    * Enforce absolute path requirement for rattest experiment binaries
    
    * Allow name on PATH

--- python/rattest/__init__.py
+++ python/rattest/__init__.py
@@ -112,7 +112,10 @@ class RatTest:
         if rat_bin is None:
             self.rat_bin = os.path.abspath(os.path.join(os.environ['RATROOT'], 'bin', 'rat'))
         else:
-            self.rat_bin = rat_bin
+            self.rat_bin = shutil.which(rat_bin)
+            if not self.rat_bin:
+                raise RuntimeError(f"Experiment binary not found: {rat_bin}. "
+                                   f"Expected a path to the binary or a name on PATH.")
 
         # Set the event file basename (rat appends .root or .ntuple.root automatically)
         self.event_file_basename = 'mc_events'
